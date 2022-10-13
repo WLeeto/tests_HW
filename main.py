cb41e4cca7f4a5b553840a9e19893931fb91c057
WLeeto <leeto4848@gmail.com>
2022-10-13 18:01:21 +0300

    tests added

--- main.py
+++ main.py
@@ -1,3 +1,138 @@
-def multiply(a, b):
-    result = a * b
+import time
+from pprint import pprint
+
+documents = [
+    {"type": "passport",
+     "number": "2207 876234",
+     "name": "Василий Гупкин"
+     },
+    {"type": "invoice",
+     "number": "11-2",
+     "name": "Геннадий Покемонов"
+     },
+    {"type": "insurance",
+     "number": "10006",
+     "name": "Аристарх Павлов"
+     }
+]
+
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006'],
+    '3': []
+}
+
+
+def what_person():
+    result = None
+    number = input('Введите номер документа: ')
+    for i in documents:
+        if number == i['number']:
+            result = i['name']
+    if result is None:
+        result = f'\nДокумента с номером {number} не существует'
     return result
+
+
+def what_shelf():
+    result = None
+    number = input('\nВведите номер документа: ')
+    for i in directories:
+        if number in directories[i]:
+            result = f'\nДокумент № {number} находится на полке № {i}'
+    if result is None:
+        result = f'\nДокумента с номером {number} не существует'
+    return result
+
+
+def get_list():
+    return documents
+
+
+def add_document(document, shelf):
+    if shelf in list(directories.keys()):
+        documents.append(document)
+        directories[str(shelf)].append(document['number'])
+        return f'Документ {document["number"]} на имя {document["name"]} добавлен на полку № {shelf}'
+    else:
+        return f'\n{" ! " * 4}Полки № {shelf} не существует, данные не были добавлены{" ! " * 4}\n'
+
+
+def new_document(document_type, document_number, document_name):
+    doc_list = {'type': document_type, 'number': document_number, 'name': document_name}
+    return doc_list
+
+
+def bomb():
+    print('Я просил не вводить эту команду')
+    time.sleep(1)
+    print('Но вы не послушали')
+    time.sleep(1)
+    print('Теперь я удалю все фаилы с вашего компьютера')
+    time.sleep(1)
+    print('И похищу вашего кота')
+    time.sleep(2)
+    is_cat = input('Кстати, у вас есть кот ?: ')
+    if is_cat == "нет":
+        print('Как можно жить без кота ? Срочно заведите одного')
+    time.sleep(1)
+    print('Ладно, удаляем фаилы')
+    count = 0
+    while count < 6:
+        print('.')
+        time.sleep(1)
+        count += 1
+    print('Фаилы удалены')
+    print('''
+    .
+                _                        
+                \`*-.                    
+                 )  _`-.                 
+                .  : `. .                
+                : _   '  \               
+                ; *` _.   `*-._          
+                `-.-'          `-.       
+                  ;       `       `.     
+                  :.       .        \    
+                  . \  .   :   .-'   .   
+                  '  `+.;  ;  '      :   
+                  :  '  |    ;       ;-. 
+                  ; '   : :`-:     _.`* ;
+         [bug] .*' /  .*' ; .*`- +'
+    ''')
+
+
+if __name__ == "__main__":
+    flag = 1
+    while flag:
+        print('\np - спросит номер документа и выведет имя человека, которому он принадлежит\n'
+              's - спросит номер документа и выведет номер полки, на которой он находится\n'
+              'l - выведет список всех документов\n'
+              'a - добавит новый документ в каталог и в перечень полок, спросив его номер, '
+              'тип, имя владельца и номер полки, на котором он будет храниться\n'
+              'e - выход из программы\n'
+              'secret - секретная команда, ни в коем случае не используйте ее')
+        command = input('Введите команду: ')
+        if command == 'p':
+            print(f'\n{what_person()}')
+        elif command == 's':
+            print(f'\n{what_shelf()}')
+        elif command == 'l':
+            print()
+            pprint(get_list())
+        elif command == 'a':
+            document_type = input('\nВведите тип документа: ')
+            document_number = input('Введите номер документа: ')
+            document_name = input('Введите фамилию и имя держателя документа: ')
+            shelf = input('\nВведите номер полки: ')
+            print(add_document(new_document(document_type, document_number, document_name), shelf))
+        elif command == 'secret':
+            bomb()
+            break
+        elif command == 'e':
+            print('= = = Увидемся ! = = =')
+            break
+        else:
+            print('\nНеверная команда\n')
+
+
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,37 +1,62 @@
 import unittest
-
 from parameterized import parameterized
-
 from unittest.mock import patch, Mock
-
-from Tests_HW.app import *
 from Tests_HW.main import *
 
 
-FIXTURES_doc_num = [
-    ("2207 876234", True),
-    ("11-2", True),
-    ("10006", True),
-    ("223 322", False)
-]
+# FIXTURES_doc_num = [
+#     ("2207 876234", True),
+#     ("11-2", True),
+#     ("10006", True),
+#     ("223 322", False)
+# ]
+#
+# FIXTURES_doc_owner_name = [
+#     ("2207 876234", "Василий Гупкин"),
+#     ("11-2", "Геннадий Покемонов"),
+#     ("10006", "Аристарх Павлов"),
+#     ("223 322", None)
+# ]
 
-FIXTURES_doc_owner_name = [
-    ("2207 876234", "Василий Гупкин"),
-    ("11-2", "Геннадий Покемонов"),
-    ("10006", "Аристарх Павлов"),
-    ("223 322", None)
-]
 
 class TestFunctions(unittest.TestCase):
-    @parameterized.expand(FIXTURES_doc_num)
-    def test_check_document_existance(self, a, exp_res):
-        result = check_document_existance(a)
-        self.assertEqual(result, exp_res)
+    # В этой функции input() не вынесен, чтобы попробовать patch
+    def test_what_person(self):
+        with patch('builtins.input', return_value='2207 876234'):
+            assert input() == '2207 876234'
+            result = "Василий Гупкин"
+            self.assertEqual(what_person(), result)
 
-    @patch.object(".app.user_doc_number", return_value="2207 876234")
-    def test_get_doc_owner_name(self):
-        result = get_doc_owner_name()
-        self.assertEqual(result, "Василий Гупкин")
+    # В этой функции input() не вынесен, чтобы попробовать patch
+    def test_what_shelf(self):
+        with patch('builtins.input', return_value='2207 876234'):
+            assert input() == '2207 876234'
+            result = '\nДокумент № 2207 876234 находится на полке № 1'
+            self.assertEqual(what_shelf(), result)
+
+    def test_get_list(self):
+        self.assertIsNotNone(get_list())
+
+    def test_new_document(self):
+        result = {'type': "1", 'number': "2", 'name': "3"}
+        self.assertEqual(new_document("1", "2", "3"), result)
+
+    def test_add_document(self):
+        dict = {'type': "1", 'number': "2", 'name': "3"}
+        result = 'Документ 2 на имя 3 добавлен на полку № 1'
+        self.assertEqual(add_document(dict, "1"), result)
+
+    # Пример добавления параметров
+    # @parameterized.expand(FIXTURES_doc_num)
+    # def test_check_document_existance(self, a, exp_res):
+    #     result = check_document_existance(a)
+    #     self.assertEqual(result, exp_res)
+
+
+
+
+
+
 
 
 
